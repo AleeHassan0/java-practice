560448302528905b648801b09abe1adb8d505399
Ali Hassan <hassan.jutt646464@gmail.com>
2026-02-17 16:06:06 +0100

    how to take input from the user in the terminal

---
+++ src/input.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);//new we have taken input but still not store
+   String name=sc.nextLine(); //now we have store the input into name
+   System.out.println(name); //just next world will print first world of the line only..if you want to print whole line then you have to write nextline.....
+    }
+}
