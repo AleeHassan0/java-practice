e8626764715a1de99ce4985e816139b105ab79ba
Ali Hassan <hassan.jutt646464@gmail.com>
2026-02-18 17:38:54 +0100

    switch

--- src/conditionals.java
+++ src/conditionals.java
@@ -10,18 +10,26 @@ public class conditionals {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter your age");
         int age = sc.nextInt();
+        // if (age > 18) {
+        //     System.out.println("adult");
+        // } else
+        //     System.out.println("not adult");
+        // if (age % 2 == 0) {
+        //     System.out.println("even number");
+        // } else
+        //     System.out.println("odd number");
 
-        if (age > 18) {
-            System.out.println("adult");
-
-        } else
-            System.out.println("not adult");
-
-        if (age % 2 == 0) {
-            System.out.println("even number");
-
-        } else
-            System.out.println("odd number");
+            //switch
+switch (age) {
+    case 1:
+        System.out.println("hello");
+        break;
+        case 2:
+            System.out.println("byee");
+            break;
+    default:System.out.println("ivalid number");
+        
+}
 
     }
 }
