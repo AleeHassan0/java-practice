ddcc449d4146b7b4ac621eb27a6da46617c6b937
Ali Hassan <hassan.jutt646464@gmail.com>
2026-02-18 17:14:22 +0100

    if else condition

---
+++ src/conditionals.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+//if else
+//else if
+//switch
+//break
+public class conditionals {
+    public static void main(String[] args) {
+      Scanner sc= new Scanner(System.in); 
+      System.out.println("enter your age"); 
+      int age=sc.nextInt();
+
+      if (age>18){
+        System.out.println("adult");
+
+      }else
+        System.out.println("not adult");
+
+
+
+
+
+
+        
+    }
+}
--- src/input.java
+++ src/input.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class input {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);//new we have taken input but still not store
+        Scanner sc =new Scanner(System.in);//now we have taken input but still not store
    String name=sc.nextLine(); //now we have store the input into name
    System.out.println(name); //just next world will print first world of the line only..if you want to print whole line then you have to write nextline.....
     }
