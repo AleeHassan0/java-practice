6cdb38f325f67c5d787b10f76e9b9222fc156be6
Ali Hassan <hassan.jutt646464@gmail.com>
2026-02-18 17:57:25 +0100

    while loop

--- src/loops.java
+++ src/loops.java
@@ -1,9 +1,14 @@
 public class loops {
     public static void main(String[] args) {
-        for(int i=0;i<3;i++){
+        // for(int i=0;i<3;i++){
+        //     System.out.println(i);
+        //     System.out.println("hello word");  
+        // }
+
+        int i=0;
+        while(i<10){
             System.out.println(i);
-            System.out.println("hello word");
-            
+i++;
         }
     }
 }
