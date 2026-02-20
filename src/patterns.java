bb539b4fef91880cdd465bac3e761d822c15d731
Ali Hassan <hassan.jutt646464@gmail.com>
2026-02-21 00:45:46 +0100

    half pyramid with the help of nested loop

--- src/patterns.java
+++ src/patterns.java
@@ -13,21 +13,32 @@ public class patterns {
     // }
 
 
-    //hollow rectangle with the help of nested loop
-   
+//hollow rectangle with the help of nested loop
+// int n=4;
+// int m=5;
+// for (int i=1;i<=n;i++){
+//     for(int j=1;j<=m;j++){
+//   if(i==1||j==1||i==n||j==m){
+//     System.out.print("*");
+// } else {
+//     System.out.print(" ");
+// }
+//     }
+//     System.out.println();
+// }
+
+
+//half pyramid
 int n=4;
-int m=5;
-for (int i=1;i<=n;i++){
-    for(int j=1;j<=m;j++){
-  if(i==1||j==1||i==n||j==m){
-    System.out.print("*");
-} else {
-    System.out.print(" ");
-}
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        System.out.print("*");
     }
     System.out.println();
-
 }
+
+
+
 
 
    }
