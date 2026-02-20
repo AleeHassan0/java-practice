68fb7dcfccecbe2da7b07d21f94a2bf4ff1f56e1
Ali Hassan <hassan.jutt646464@gmail.com>
2026-02-20 21:40:43 +0100

    hollow rectangle with the help of nested loop

--- src/patterns.java
+++ src/patterns.java
@@ -1,11 +1,34 @@
 public class patterns {
+
+    //rows outer loops
+    //colomns inner loops
    public static void main(String[] args) {
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            System.out.print("*");
-        }
-        System.out.println();
-      
+
+//for loop ex.1
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<4;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //hollow rectangle with the help of nested loop
+   
+int n=4;
+int m=5;
+for (int i=1;i<=n;i++){
+    for(int j=1;j<=m;j++){
+  if(i==1||j==1||i==n||j==m){
+    System.out.print("*");
+} else {
+    System.out.print(" ");
+}
     }
+    System.out.println();
+
+}
+
+
    }
 }
