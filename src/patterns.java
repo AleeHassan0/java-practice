9b71ca55cdfaf0c463d9bede25f11cf8e78fa406
Ali Hassan <hassan.jutt646464@gmail.com>
2026-02-25 00:01:02 +0100

    floyd's triangle with the help of nested loop

--- src/patterns.java
+++ src/patterns.java
@@ -80,14 +80,26 @@ public class patterns {
 
 
 //inverted half pyramid with numbers
+// int n=5;
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=n-i+1;j++){
+//         System.out.print(j);
+//     }
+//     System.out.println();
+// }
+
+
+
+//Floyd's triangle
 int n=5;
+int number =1;
 for(int i=1;i<=n;i++){
-    for(int j=1;j<=n-i+1;j++){
-        System.out.print(j);
+    for(int j=1;j<=i;j++){
+System.out.print(number+" ");
+number++;
     }
     System.out.println();
 }
-
 
 
 
