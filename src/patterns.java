a92cedeac8fb48beaecd96caa23a038de63ba76e
Ali Hassan <hassan.jutt646464@gmail.com>
2026-02-24 23:48:20 +0100

    inverted half pyramid with numbers with the help of nested loop

--- src/patterns.java
+++ src/patterns.java
@@ -64,14 +64,35 @@ public class patterns {
 // System.out.println();
 // }
 
+
+
+
+
+// int n=5;
+// for(int i=1;i<=n;i++){
+//    //inner loop for number
+//    for(int j=1;j<=i;j++){
+//     System.out.print(j+" ");
+//    }  
+//   System.out.println(" ");
+// }
+
+
+
+//inverted half pyramid with numbers
 int n=5;
 for(int i=1;i<=n;i++){
-   //inner loop for number
-   for(int j=1;j<=i;j++){
-    System.out.print(j+" ");
-   }  
-  System.out.println(" ");
+    for(int j=1;j<=n-i+1;j++){
+        System.out.print(j);
+    }
+    System.out.println();
 }
+
+
+
+
+
+
 
 
 
